9812223922b6df2078aad03e888191bf4649f8c1
Nils <code.by.nils@gmail.com>
2025-10-21 13:46:42 +0200

    feat(services): skeleton of the Java Spring Boot services
    Spring Boot structure for api, ingest and processor

---
+++ services/ingest/news-radar-ingest/src/test/java/io/github/codebynils/news_radar_ingest/NewsRadarIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.codebynils.news_radar_ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsRadarIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/processor/news-radar-processor/src/test/java/io/github/codebynils/news_radar_processor/NewsRadarProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.codebynils.news_radar_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsRadarProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
